ef482d793349a6d0cb1019fbe9449559280a8ea6
lolpointdeux <75392240+lolpointdeux@users.noreply.github.com>
2021-10-07 13:27:35 +0200

    Ajoute de l'Ago ex comparaison

---
+++ AlgoRomeo/AlgoRomeo/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoRomeo
+{
+    class Class1
+    {
+        public void calcul(int age , out string awnser)
+        {
+            if (age > 14 || age < 6 )
+            {
+                awnser = "Vous n'avez pas l'age requis";
+            }
+            else if (age >= 12)
+            {
+                awnser = "Vous etes cadet ";
+            }
+            else if (age >= 10)
+            {
+                awnser = "Vous etes un Minime";
+            }
+            else if (age >= 8)
+            {
+                awnser = "Vous êtes un pupil";
+            } else
+                awnser = "Vous êtes un Poussin";
+        }
+      
+
+       
+    }
+}
---
+++ AlgoRomeo/AlgoRomeo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlgoRomeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string awnser;
+           
+            Console.WriteLine("Quel age avez-vous ?");
+            age = int.Parse((Console.ReadLine()));
+
+            Class1 class1 = new Class1();
+
+            class1.calcul(age, out awnser);
+
+            Console.WriteLine(awnser);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
