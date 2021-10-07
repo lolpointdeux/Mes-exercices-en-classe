1e8a6e3b509575883362344f0e0d6234f9087640
lolpointdeux <75392240+lolpointdeux@users.noreply.github.com>
2021-10-07 13:48:11 +0200

    Table de 5

---
+++ Table5/Table5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Table5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int incr = 0;
+            string chaine = "";
+
+            do
+            {
+                chaine =  chaine + (incr * 5) + " " ;
+                incr++;
+               
+            } while (incr <= 100);
+
+            Console.WriteLine(chaine);
+            Console.ReadLine();
+            
+        }
+        
+
+    }
+}
