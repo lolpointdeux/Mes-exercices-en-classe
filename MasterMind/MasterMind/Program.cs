39ca82d0452518f628b62e412654e3825624f611
LOL.2 <75392240+lolpointdeux@users.noreply.github.com>
2021-11-18 14:23:12 +0100

    Add files via upload

--- MasterMind/MasterMind/Program.cs
+++ MasterMind/MasterMind/Program.cs
@@ -6,26 +6,64 @@ namespace MasterMind
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entrez les couleur que vous avez choisit");
-            
+            int[] combiJ = new int[4];
             int[] combi = new int[4];
-            masteryi(combi);
+
+            for (int c = 0; c < 4; c++)
+            {
+                Console.WriteLine($"Entrez la {c} couleur que vous avez choisit");
+                combiJ[c] = int.Parse(Console.ReadLine());
+            }
+                   
+            masterRandom(combi);
+            pionRouge(combi, combiJ);
             afficher(combi);
+            
 
         }
-        public static void masteryi(int[] combi)
+        public static void masterRandom(int[] combi)
         {
             Random rnd = new Random();
 
             for (int n = 0; n < combi.Length; n++)
             {
-                int nombre = rnd.Next(1, 6);
+                int nombre = rnd.Next(1, 6);            
                 combi[n] = nombre;
+
+            }
+        }
+        public static void pionRouge(int [] combi, int[] combiJ)
+        {
+            int rouge = 0;
+            for (int i = 0; i < combi.Length; i++)
+            {
+                
+                if (combi[i] == combiJ[i])
+                {
+                    rouge = rouge + 1;
+                }
+            }
+       
+
+        }
+        public static void pionBlanc(int[] combi, int[] combiJ)
+        {
+            int blanc = 0;
+            for (int i = 0; i < combi.Length; i++)
+            {
+                for (int j = 0; j <combi.Length; j++)
+                {
+                    if (combi[i] == combiJ[j])
+                    {
+                        blanc = blanc + 1;
+                    }
+                }
+               
             }
         }
         public static void afficher (int [] combi)
         {
-            for (int b = 0; b < 3; b++)
+            for (int b = 0; b < combi.Length; b++)
             {
                 Console.Write(combi[b] + " ");
                 
