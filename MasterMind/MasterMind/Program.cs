971cd4d54676412781bd52a2bb8bcca86eb837db
lolpointdeux <180150@site.asty-moulin.be>
2021-12-24 11:54:59 +0100

    Mise à jour

--- MasterMind/MasterMind/Program.cs
+++ MasterMind/MasterMind/Program.cs
@@ -2,68 +2,122 @@
 
 namespace MasterMind
 {
+    public static class Global
+    {
+        //Variable globale qui définit la longeur du tableau
+        public static int longeur = 4;
+    }
     class Program
     {
+       
         static void Main(string[] args)
         {
             int[] combiJ, clone, combi ;
-            
-           
-            string proposition;
+            int chances,rouge,blanc,dummy;
+            string chanceUser, proposition,reponse;           
 
-            
-            
-            Console.WriteLine($"Entrez 4 chiffres que vous avez choisit");
-            proposition = Console.ReadLine();
-
-            StringToArray(proposition, out combiJ);
+            //Utilisation de la fonction qui randomise
             masterRandom(out combi);
-            Clonnage(combi, out clone);
-            pionRouge(combi, combiJ);
-            afficher(combi);
-            
+            //Back-up du tableau
+            Clonnage(combi, out clone, out reponse);
+
+            //Nombres de chances  + Vérrication des imputs
+            do
+            {
+                Console.WriteLine("Combien de chance voulez-vous ?");
+                chanceUser = Console.ReadLine();
+            } while (!int.TryParse(chanceUser, out chances));
+
+            //Boucle princiaple 
+            do
+            {
+                //Encodage de la proposition de l'utilisateur + Vérrication des imputs 
+                do
+                {
+                    Console.WriteLine($"Entrez {Global.longeur} chiffres que vous avez choisit");
+                    proposition = Console.ReadLine();
+                } while (!int.TryParse(proposition,out dummy) || proposition.Length != Global.longeur);
+               
+                StringToArray(proposition, out combiJ);
+                //Comptabilise les pions rouges/blancs
+                pionRouge(combi, combiJ, out rouge);
+                pionBlanc(combi, combiJ, out blanc);
+                afficher(blanc, rouge);
+                chances--;
+                ClonnageInv(clone, out combi);
+            } while (rouge != Global.longeur && chances > 0);
+
+            //Résulat final 
+            if (rouge == Global.longeur)
+            {
+                Console.WriteLine("Tu es trop fort !!! ");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"t'es nul, le nombre à deviner était {reponse} ");
+                Console.ReadLine();
+            }
 
         }
        
+        //Convertit les imputs en tableau
         static void StringToArray(string proposition, out int[] combiJ)
         {
-            combiJ = new int[4];
+            combiJ = new int[Global.longeur];
 
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; i < combiJ.Length; i++)
             {
                 combiJ[i] = int.Parse(proposition[i].ToString());
             }
         }
+
+        //Randomizer 
         public static void masterRandom(out int[] combi)
         {
-            combi = new int[4];
+            combi = new int[Global.longeur];
             Random rnd = new Random();
 
             for (int n = 0; n < combi.Length; n++)
             {
                 int nombre = rnd.Next(1, 6);            
                 combi[n] = nombre;
+            }
+        }
+
+        //Restauration du back-up
+        public static void ClonnageInv(int[] clone, out int[] combi)
+        {
+            combi = new int[Global.longeur];
+            for (int i = 0; i < clone.Length; i++)
+            {
+                combi[i] = clone[i];
 
             }
         }
-         public static void Clonnage(int [] combi, out int [] clone )
+
+        //Back-up du tableau
+        public static void Clonnage(int[] combi, out int[] clone,out string reponse)
         {
-            clone = new int[4];         
+            clone = new int[Global.longeur];
+            reponse = "";
             for (int i = 0; i < clone.Length; i++)
             {
                 clone[i] = combi[i];
-
-            }
+                reponse = reponse + clone[i] + "";
+            }      
         }
-        public static void pionRouge(int [] combi, int[] combiJ)
+
+        //Comptabilise les pions rouges
+        public static void pionRouge(int[] combi, int[] combiJ, out int rouge)
         {
-            int rouge = 0;
+             rouge = 0;
+           
             
             for (int i = 0; i < combi.Length; i++)
             {
-                
-
-                if (combi[i] == combiJ[i])
+    
+                if (combiJ[i] == combi[i])
                 {
                     rouge = rouge + 1;
                     combiJ[i] = -1;
@@ -73,9 +127,11 @@ namespace MasterMind
        
 
         }
-        public static void pionBlanc(int[] combi, int[] combiJ)
+
+        //Comptabilise les pions blancs
+        public static void pionBlanc(int[] combi, int[] combiJ,out int blanc)
         {
-            int blanc = 0;
+            blanc = 0;
             for (int i = 0; i < combi.Length; i++)
             {
                 for (int j = 0; j <combi.Length; j++)
@@ -83,20 +139,19 @@ namespace MasterMind
                     if (combi[i] == combiJ[j])
                     {
                         blanc = blanc + 1;
-                        combiJ[i] = -1;
+                        combiJ[j] = -1;
                     }
                 }
                
             }
         }
-        public static void afficher (int [] combi)
+
+        //Affichage intérmédiaire
+        public static void afficher (int blanc, int rouge)
         {
-            for (int i = 0; i < combi.Length; i++)
-            {
-                Console.Write(combi[i] + " ");
-                
-            }
-            Console.WriteLine();
+            Console.WriteLine($"Voici le nombre de pions blanc {blanc}");
+            Console.WriteLine($"Voici le nombre de pions rouge {rouge}");          
+            
         }
        
 
